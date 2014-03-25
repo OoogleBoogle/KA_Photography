a37ef92a37e0b140daa7058e4478a899da79031f
OoogleBoogle <simattfield@gmail.com>
2014-03-25 00:30:44 +0000

    functional menu for galleries. fixed mobile-landscape menu.

--- js/page.js
+++ js/page.js
@@ -1,10 +1,18 @@
+$('#food').hide();
 
 $(document).ready(function() {
-	$('.icon-menu').on('click', function() {
-		$('#sidebar').fadeIn();
+	$('.icon-menu').on('click', function(e) {
+		e.preventDefault();
+		$('#sidebar').animate({'left': 0});
 	});
-	$('#sidebar li').on('click', function() {
-		$('#sidebar').fadeOut();
+	$('#food-link').on('click', function() {
+		$('#sidebar').animate({left: "-=78%"});
+		$('#interiors').delay(300).fadeOut();
+		$('#food').delay(300).fadeIn();
 	});
-
+	$('#interior-link').on('click', function() {
+		$('#sidebar').animate({left: "-=78%"});
+		$('#food').delay(300).fadeOut();
+		$('#interiors').delay(300).fadeIn();
+	});
 });
