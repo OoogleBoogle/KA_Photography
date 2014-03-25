519b6165e3aea373870425f3d848022bd0229d8e
OoogleBoogle <simattfield@gmail.com>
2014-03-25 12:47:39 +0000

    basic gallery pages complete over all devices

--- js/page.js
+++ js/page.js
@@ -5,14 +5,19 @@ $(document).ready(function() {
 		e.preventDefault();
 		$('#sidebar').animate({'left': 0});
 	});
-	$('#food-link').on('click', function() {
+	$('.food-link').on('click', function() {
 		$('#sidebar').animate({left: "-=78%"});
 		$('#interiors').delay(300).fadeOut();
 		$('#food').delay(300).fadeIn();
 	});
-	$('#interior-link').on('click', function() {
+	$('.interior-link').on('click', function() {
 		$('#sidebar').animate({left: "-=78%"});
 		$('#food').delay(300).fadeOut();
 		$('#interiors').delay(300).fadeIn();
 	});
+	$('#topbar').on('click', 'a', function() {
+		$('a').removeClass('selected');
+		$(this).addClass('selected');
+	})
+
 });
